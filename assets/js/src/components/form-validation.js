5721edcdb420957e8ad8ec1cdaca51e71622ff8f
vladislavponomaryov <vladislavponomaryov96@gmail.com>
2023-10-11 14:58:38 +0600

    Добавить функционал в expo cloud с оповещением в табах не заполненных полей

--- assets/js/src/components/form-validation.js
+++ assets/js/src/components/form-validation.js
@@ -1,25 +1,66 @@
 /**
  * Form validation
-*/
+ */
 
 const formValidation = (() => {
+	const selector = 'needs-validation'
 
-  const selector = 'needs-validation';
+	window.addEventListener(
+		'load',
+		() => {
+			// Fetch all the forms we want to apply custom Bootstrap validation styles to
+			let forms = document.getElementsByClassName(selector)
+			// Loop over them and prevent submission
+			let validation = Array.prototype.filter.call(forms, form => {
+				form.addEventListener(
+					'submit',
+					e => {
+						if (form.checkValidity() === false) {
+							e.preventDefault()
+							e.stopPropagation()
+						}
+						form.classList.add('was-validated')
+						tabValidation(e)
+					},
+					false
+				)
+			})
+		},
+		false
+	)
 
-  window.addEventListener('load', () => {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    let forms = document.getElementsByClassName(selector);
-    // Loop over them and prevent submission
-    let validation = Array.prototype.filter.call(forms, (form) => {
-      form.addEventListener('submit', (e) => {
-        if (form.checkValidity() === false) {
-          e.preventDefault();
-          e.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
+	function tabValidation(e) {
+		const invalidTabsName = getInvalidTabsName(e)
 
-export default formValidation;
+		showTabError(invalidTabsName)
+	}
+
+	function showTabError(invalidTabsName) {
+		const tabs = document.querySelectorAll('.nav.nav-tabs [role="tab"]')
+
+		tabs.forEach(tab => {
+			if (invalidTabsName.includes(tab.id)) {
+				tab.classList.add('invalid')
+			} else {
+				tab.classList.remove('invalid')
+			}
+		})
+	}
+
+	function getInvalidTabsName(e) {
+		const form = e.srcElement
+		const invalidElements = form.querySelectorAll('.was-validated .form-control:invalid')
+		const invalidTabsName = []
+
+		invalidElements.forEach(element => {
+			const tabName = element.closest('[role="tabpanel"]').getAttribute('id')
+			invalidTabsName.push(tabName + '-tab')
+		})
+
+		return invalidTabsName
+	}
+
+	return false
+})()
+
+export default formValidation
